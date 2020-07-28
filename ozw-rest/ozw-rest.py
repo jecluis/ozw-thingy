ca1fb40739c88decb34bca6cc10ebe3ab70f9bdd
Joao Eduardo Luis <joao@wipwd.dev>
2020-07-28 12:53:35 +0000

    ozw-rest: support CORS for frontend testing
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- ozw-rest/ozw-rest.py
+++ ozw-rest/ozw-rest.py
@@ -19,6 +19,7 @@ from pydispatch import dispatcher
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.controller import Controller, controller
 from backend.api import nodes
@@ -26,6 +27,20 @@ from backend.api import network as api_network
 
 
 app = FastAPI()
+
+origins = [
+    'http://localhost',
+    'http://192.168.1.88:4200',
+    'http://192.168.1.75'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
